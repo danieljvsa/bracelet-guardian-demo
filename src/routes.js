526a8389888a0ea77dc9049d371a2a7fa75b6686
danieljvsa <danielviana18@gmail.com>
2022-04-21 15:29:23 +0100

    Login System added. First security layer added.

--- src/routes.js
+++ src/routes.js
@@ -5,23 +5,33 @@ const ProfileController = require('./controllers/ProfilesControllers')
 const BraceletController = require('./controllers/BraceletsControllers')
 const NursesController = require('./controllers/NursesControllers')
 const PatientsController = require('./controllers/PatientsControllers')
+const UsersController = require('./controllers/UsersCrontrollers')
+const authMiddleware = require('./middlewares/auth')
+
+
+
+routes.get('/users', authMiddleware, UsersController.index)
+routes.post('/create', UsersController.create)
+routes.post('/authenticate', UsersController.authenticate)
+routes.post('/forgot-password', UsersController.forgot_password)
+routes.post('/reset-password', UsersController.reset_password)
 
 routes.get('/', ProfileController.index) //web routes
-routes.get('/get-profiles', ProfileController.get) //web routes
-routes.post('/add-profile', ProfileController.create) //web routes
-routes.put('/profiles/:id', ProfileController.update) //web routes
-routes.delete('/profiles/:id', ProfileController.delete) //web routes
+routes.get('/get-profiles', authMiddleware, ProfileController.get) //web routes
+routes.post('/add-profile', authMiddleware, ProfileController.create) //web routes
+routes.put('/profiles/:id', authMiddleware, ProfileController.update) //web routes
+routes.delete('/profiles/:id', authMiddleware, ProfileController.delete) //web routes
 
-routes.get('/get-bracelet', BraceletController.get) //web routes
-routes.post('/add-bracelet', BraceletController.create) //esp32 routes
-routes.delete('/bracelets/:id', BraceletController.delete) //web routes
+routes.get('/get-bracelet', authMiddleware, BraceletController.get) //web routes
+routes.post('/add-bracelet', authMiddleware, BraceletController.create) //esp32 routes
+routes.delete('/bracelets/:id', authMiddleware, BraceletController.delete) //web routes
 
-routes.get('/get-nurses', NursesController.get) //web routes
-routes.post('/add-nurse', NursesController.create) //web routes
-routes.put('/nurses/:id', NursesController.update) //web routes
-routes.delete('/nurses/:id', NursesController.delete) //web routes
+routes.get('/get-nurses', authMiddleware, NursesController.get) //web routes
+routes.post('/add-nurse', authMiddleware, NursesController.create) //web routes
+routes.put('/nurses/:id', authMiddleware, NursesController.update) //web routes
+routes.delete('/nurses/:id', authMiddleware, NursesController.delete) //web routes
 
-routes.get('/patients/:id', PatientsController.get) //web routes
+routes.get('/patients/:id', authMiddleware, PatientsController.get) //web routes
 routes.post('/alert', PatientsController.create) //esp32 routes
 routes.post('/test-alert', PatientsController.test_create) //esp32 routes
 routes.post('/test', PatientsController.test) //esp32 test connection route
