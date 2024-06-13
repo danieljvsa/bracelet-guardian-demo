00f043347004ff868910ee8aeb79e7ff70089ae8
danieljvsa <danielviana18@gmail.com>
2024-06-13 21:19:28 +0100

    Add future paramenters to bracelets table

--- src/database/migrations/0004_bracelets.js
+++ src/database/migrations/0004_bracelets.js
@@ -3,6 +3,9 @@ exports.up = async function(knex) {
     await knex.schema.createTable('bracelets',function(table){
         table.increments('braceletId').primary();
         table.string('macAddress').notNullable();
+        table.string('imei')
+        table.string('battery').defaultTo(0)
+        table.string('port')
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
         table.integer('patientId').unsigned().index().references('patientId').inTable('patients');
