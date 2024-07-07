17f9d2ac35a979a255195829779ce9158c7b1cd3
danieljvsa <danielviana18@gmail.com>
2024-07-07 23:36:56 +0100

    Add ops routes to api

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/auth', routes.auth)
+app.use('/ops/v1', routes.ops)
 app.use('/api/v1', routes.api)
 app.use('/admin', routes.admin)
 
