a1dfb2aa5368fdc58473632cc035805c590680e5
danieljvsa <danielviana18@gmail.com>
2024-07-07 23:27:35 +0100

    Add organizations routes to admin and api

---
+++ src/controllers/organizations.js
@@ -0,0 +1,78 @@
+const knex = require('../database');
+const logic = require('../logic')
+
+module.exports.get = async (req, res) => {
+    return res.send({success: true, data: req.org})
+}
+
+module.exports.getAll = async (req, res) => {
+    return res.send({success: true, data: req.orgs})
+}
+
+module.exports.create = async (req, res) => {
+    const { name, code} = req.body;
+    if(typeof name !== "string" || typeof code !== "string") return res.send({success: false, error: "name or code missing."})
+    
+    try {
+        await knex('organizations').insert({
+            name: name,
+            email: email,
+            orgId: orgId,
+            active: true,
+            isAdmin: (org[0].code === "admin") ? true : false,
+            createdBy: req.user.id
+        })
+
+        const org = await knex('organizations').where({name: name, code: code})
+        if(!org.length) return res.send({success: false, error: "Organization not created."})
+        
+        return res.send({success: true, data: org[0].id})
+    } catch (error) {
+        console.log("controllers/organizations/create: ", error)
+        return res.send({success: false, error: error})
+    }
+} 
+
+module.exports.update = async (req, res) => {
+    const { name, code } = req.body;
+
+    try {
+
+        await knex('organizations') 
+        .update({ 
+            name: name ? name : req.org.name, 
+            code: code ? code : req.org.code
+        })
+        .where({id: req.org.id}) 
+        .catch((error) => {
+            console.log("controllers/organizations/update", error)
+            return res.send({success: false, error})
+        })
+        return res.send({success: true, data: req.org.id})
+    } catch (error) {
+        console.log("controllers/organizations/update: ", error)
+        return res.send({success: false, error: error})
+    }
+    
+}
+
+module.exports.generateApiKey = async (req, res) => {
+    try {
+        const key = await logic.master.generateUUID()
+        if(!key.success) return res.send(key)
+
+        const hashKey = await logic.master.hashNumber(key)
+        if(!hashKey.success) return res.send(hashKey)
+
+        await knex('organizations').update({apiKey: hashKey}).where({orgId: req.org.orgId})
+        .catch((error) => {
+            console.log("controllers/organizations/generateApiKey", error)
+            return res.send({success: false, error})
+        })
+        
+        return res.send({success: true, data: req.org.id})
+    } catch (error) {
+        console.log("controllers/organizations/generateApiKey: ", error)
+        return res.send({success: false, error: error})
+    }
+}
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,8 +1,53 @@
 const jwt = require('jsonwebtoken')
 const knex = require('../database')
+const logic = require('../logic')
 
 
 module.exports.validateUser = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if(!authHeader){
+        return res.send({success: false, error: 'No token provided'})
+    }
+
+    const parts = authHeader.split(' ')
+
+    if(!parts.length === 2 ){
+        return res.send({success: false, error: 'token error'})
+    }
+    
+    const [scheme, token] = parts
+
+    if(!/^Bearer$/i.test(scheme)){
+        return res.send({success: false, error: 'token malformatted'})
+    }
+
+    jwt.verify(token, process.env.JWT_KEY, async (err, decoded) => {
+        if(err) return res.send({success: false, error: 'token invalid'})
+    
+        req.userId = decoded.id
+        const user = await knex('users').where({
+            id: decoded.id
+        })
+
+        if (user.length === 0) return res.send({success: false, error: 'That user is not registered'})
+        
+        if(!user[0].active) return res.send({success: false, error: "User's account not active."})
+
+        const org = await knex('organizations').where({id: user[0].orgId})
+        if (!org.length) return res.send({success: false, error: 'No Organization found.'})
+
+        const compareApiKey = await logic.master.comparePassword(req.headers.apiKey, org[0].apiKey)
+        if(!compareApiKey.success) return res.send(compareApiKey)
+        if(!compareApiKey.data) return res.send({success: false, error: "Unauthorized"})
+
+        req.user = user[0]
+        return next()
+    })
+
+}
+
+module.exports.validateAdmin = async (req, res, next) => {
     const authHeader = req.headers.authorization
 
     if(!authHeader){
@@ -34,8 +79,14 @@ module.exports.validateUser = async (req, res, next) => {
         }
 
         if(!user[0].active) return res.send({success: false, error: "User's account not active."})
+        
+        const org = await knex('organizations').where({
+            orgId: user[0].orgId
+        })
 
-        req.user = user[0]
+        if(org.code !== "admin") return res.send({success: false, error: "Unauthorized"})
+        req.manager = user[0]
+    
         return next()
     })
 
