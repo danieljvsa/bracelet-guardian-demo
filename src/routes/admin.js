84069c77d6e26aa477925dc2653cd64a34738c0f
danieljvsa <danielviana18@gmail.com>
2024-07-14 14:54:02 +0100

    Fix route to generate api keys

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -16,6 +16,6 @@ app.route('/organizations')
     .get(middlewares.organizations.getOrganizationsList, controllers.organizations.getAll)
 
 app.route('/organization/generate/key/:orgId')
-    put(middlewares.organizations.checkByParams, controllers.organizations.generateApiKey)
+    .put(middlewares.organizations.checkByParams, controllers.organizations.generateApiKey)
 
 module.exports = app
